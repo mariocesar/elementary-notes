0ddd583aed388e9c90c77ad12476a8114610c8d8
Mario César <mariocesar.c50@gmail.com>
2020-02-25 21:37:35 -0400

    Upgrade styling

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ LICENSE:
     Boston, MA 02110-1301 USA.
 """
 
+import sys
 import os
 import gi
 
@@ -39,30 +40,38 @@ class NoteWindow(Gtk.Window):
     #text_view text {
         background: #FFFFE0;
     }
-    /*
-    #text_view text selection {
-        color: #8e8e18;
-        background: transparent;
+
+    #headerbar {
+        background-color: #FFFFE0;
+        background-image: none;
+        border-bottom-color: #FFFFE0;
+        box-shadow: none;
     }
-    */
     """
 
-    def __init__(self, application):
-        super(NoteWindow, self).__init__()
-        self.application = application
+    def __init__(self, app):
+        Gtk.Window.__init__(self)
+        self.app = app
 
+        self.set_application(app)
         self.set_name("note_window")
         self.set_title("Note")
         self.set_resizable(True)
-        self.set_keep_above(True)
         self.set_size_request(460, 500)
-        self.set_border_width(10)
         self.set_gravity(Gdk.Gravity.SOUTH_EAST)
-        self.set_type_hint(Gdk.WindowTypeHint.NORMAL)
 
         # Signals
         self.restore_position()
         self.connect("delete_event", self.save_position)
+
+        # HeaderBar
+        hb = Gtk.HeaderBar()
+        hb.set_name("headerbar")
+        hb.set_has_subtitle(False)
+        hb.set_show_close_button(True)
+        hb.set_decoration_layout("close:")
+        hb.set_title("Note")
+        self.set_titlebar(hb)
 
         # Styles
         css_provider = Gtk.CssProvider()
@@ -77,41 +86,42 @@ class NoteWindow(Gtk.Window):
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
         )
 
-        # UI
-        hb = Gtk.HeaderBar()
-        hb.set_show_close_button(True)
-        hb.props.title = "Note"
-        self.set_titlebar(hb)
-
+        # Grid
         self.grid = Gtk.Grid()
         self.grid.set_name("grid")
-        self.grid.margin_left = 20
-        self.grid.margin_right = 20
-        self.grid.margin_top = 20
-        self.grid.margin_bottom = 20
 
         self.add(self.grid)
 
         # Text View
-
         self.textview = GtkSource.View()
         self.textview.set_name("text_view")
+        self.textview.set_insert_spaces_instead_of_tabs(True)
         self.textview.set_auto_indent(True)
         self.textview.set_indent_on_tab(True)
         self.textview.set_smart_backspace(True)
         self.textview.set_smart_home_end(True)
         self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.textview.set_left_margin(15)
+        self.textview.set_right_margin(15)
+        self.textview.set_top_margin(15)
+        self.textview.set_bottom_margin(15)
 
         lang_manager = GtkSource.LanguageManager().get_default()
         language = lang_manager.get_language("markdown")
 
+        stylemanager = GtkSource.StyleSchemeManager.get_default()
+        style_scheme = stylemanager.get_scheme('solarized-light')
+
         self.textbuffer = GtkSource.Buffer.new_with_language(language)
+        self.textbuffer.set_style_scheme(style_scheme)
         self.textbuffer.connect("changed", self.on_text_buffer_change)
         self.textview.set_buffer(self.textbuffer)
 
         self.checkout_text_buffer()
 
+        # Scroll view
         scrolledwindow = Gtk.ScrolledWindow()
+        scrolledwindow.margin = 10
         scrolledwindow.set_name("scroll_view")
         scrolledwindow.set_hexpand(True)
         scrolledwindow.set_vexpand(True)
@@ -120,24 +130,24 @@ class NoteWindow(Gtk.Window):
         self.grid.attach(scrolledwindow, 0, 1, 3, 1)
 
     def checkout_text_buffer(self):
-        if os.path.exists(self.application.notes_file_path):
-            with open(self.application.notes_file_path, "r+") as f:
+        if os.path.exists(self.app.notes_file_path):
+            with open(self.app.notes_file_path, "r+") as f:
                 self.textbuffer.set_text(f.read(), f.tell())
         else:
-            open(self.application.notes_file_path, "a").close()
+            open(self.app.notes_file_path, "a").close()
 
     def commit_text_buffer(self):
         text = self.textbuffer.get_text(
             self.textbuffer.get_start_iter(), self.textbuffer.get_end_iter(), True
         )
 
-        with open(self.application.notes_file_path, "w+") as f:
+        with open(self.app.notes_file_path, "w+") as f:
             f.write(text)
 
     def restore_position(self):
         try:
-            x, y = self.application.settings["window-position"]
-            width, height = self.application.settings["window-size"]
+            x, y = self.app.settings["window-position"]
+            width, height = self.app.settings["window-size"]
         except ValueError:
             self.set_position(Gtk.WindowPosition.CENTER)
         else:
@@ -145,8 +155,8 @@ class NoteWindow(Gtk.Window):
             self.move(x, y)
 
     def save_position(self, widget, event):
-        self.application.settings["window-size"] = widget.get_size()
-        self.application.settings["window-position"] = widget.get_position()
+        self.app.settings["window-size"] = widget.get_size()
+        self.app.settings["window-position"] = widget.get_position()
 
     def on_text_buffer_change(self, widget):
         self.commit_text_buffer()
@@ -157,17 +167,12 @@ class Application(Gtk.Application):
 
     def __init__(self, *args, **kwargs):
         self.settings = Gio.Settings("org.gnome.notes")
-        self.notes_file_path = os.path.join(self.data_dir, "notes.md")
+        self.notes_file_path = os.path.expanduser("~/Dropbox/Notes/Notes.md")
 
         if not os.path.exists(self.data_dir):
             os.makedirs(self.data_dir)
 
         Gtk.Application.__init__(self, *args, **kwargs)
-
-    def create_window(self):
-        self.window = NoteWindow(self)
-        self.add_window(self.window)
-        self.window.show_all()
 
     def do_activate(self):
         windows = self.get_windows()
@@ -175,10 +180,16 @@ class Application(Gtk.Application):
         if windows:
             for window in windows:
                 window.present()
+                window.activate()
         else:
-            self.create_window()
+            window = NoteWindow(self)
+            window.show_all()
+            window.present()
+            window.activate()
+
+            self.add_window(window)
 
 
 if __name__ == "__main__":
     application = Application()
-    application.run(None)
+    application.run(sys.argv)
